756ba50b4ca12a5dc9db5483808cb29cdffd61a8
EnkiEnki77 <61898015+EnkiEnki77@users.noreply.github.com>
2023-03-09 21:40:51 -0500

    Initial commit

---
+++ Chapter 1: what is JS/ManyFaces.js
@@ -0,0 +1,11 @@
+// A paradigm is universal mindset and approach to structuring code.
+
+//The three main paradigms are procedural, OOP, and FP.
+
+// procedural orders code in a top down linear progression through a pre determined set of operations, known as procedures. 
+
+//OO collects logic and data together into classes. 
+
+// FP organizes code into pure functions, and the adaptations of those functions as values. 
+
+//JS is a multi-paradigm language, meaning you write code line by line in any paradigm you see fit. 
---
+++ README.md
@@ -0,0 +1,3 @@
+# ydkjs-get-started
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-u9bgqg)
